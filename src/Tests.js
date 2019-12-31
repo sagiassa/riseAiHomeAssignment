fc7d068637724af87da6609628c904a34bcdabbf
sagiassa <51288247+sagiassa@users.noreply.github.com>
2019-12-31 22:39:29 +0200

    First commit

---
+++ src/Tests.js
@@ -0,0 +1,23 @@
+
+let main = new Router(4);
+console.log(main.Execution);
+main.handleRequest(1);
+main.handleRequest(2);
+main.handleRequest(3);
+main.handleRequest(4);
+main.handleRequest(5);
+main.handleRequest(123);
+main.handleRequest(123);
+main.handleRequest(123);
+main.handleRequest(125);
+main.handleRequest(12);
+main.handleRequest(124);
+main.handleRequest(7);
+main.handleRequest(9);
+main.handleRequest(11);
+main.handleRequest(132);
+main.handleRequest(99);
+main.handleRequest(0);
+
+
+console.log(main.Execution);
